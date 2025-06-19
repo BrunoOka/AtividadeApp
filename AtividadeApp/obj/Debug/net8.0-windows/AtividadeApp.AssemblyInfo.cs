322a6c27212da554c313f31c69c9ad50513a9d29
bruno <brunohideki31@gmail.com>
2025-06-19 15:34:42 -0300

    criando frmretangulo

--- AtividadeApp/obj/Debug/net8.0-windows/AtividadeApp.AssemblyInfo.cs
+++ AtividadeApp/obj/Debug/net8.0-windows/AtividadeApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtividadeApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c9a00079d4aaa8ab7e5123e04c1b8381d450403")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+490947c7838bda508d55a40ceefd6134f26ae574")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtividadeApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtividadeApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
