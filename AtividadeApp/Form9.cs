ba59b3aa404a268db5e4a73f426c6077960075f5
bruno <brunohideki31@gmail.com>
2025-08-21 14:21:05 -0300

    programando btncalcular(frmrtj)

--- AtividadeApp/Form9.cs
+++ AtividadeApp/Form9.cs
@@ -16,5 +16,37 @@ namespace AtividadeApp
         {
             InitializeComponent();
         }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            double popular, geral, arquibancada, cadeiras, totpubl, rdp = 0, rdg = 0, rda = 0, rdc = 0, rendatot;
+
+            totpubl = Convert.ToDouble(txttotpub.Text);
+
+            // Cálculo da quantidade de ingressos por tipo
+
+            popular = totpubl * 0.10;
+            geral = totpubl * 0.50;
+            arquibancada = totpubl * 0.30;
+            cadeiras = totpubl * 0.10;
+
+            if (popular > 0)
+                rdp = popular * 5;
+
+            if (geral > 0)
+                rdg = geral * 10;
+
+            if (arquibancada > 0)
+                rda = arquibancada * 20;
+
+            if (cadeiras > 0)
+                rdc = cadeiras * 30;
+
+            //Cálculo da renda total
+
+            rendatot = rdp + rdg + rda + rdc;
+
+            txtrt.Text = rendatot.ToString();
+        }
     }
 }
